1ccbb8fb5143a333d55243bc8ec273d1c8173510
rnd@outlook.com <rnd@outlook.com>
2022-08-28 20:18:23 -0300

    adding fixes

--- app/src/androidTest/java/com/ronaldosanches/overlaytimer/ui/activity/TimerActivityTest.kt
+++ app/src/androidTest/java/com/ronaldosanches/overlaytimer/ui/activity/TimerActivityTest.kt
@@ -136,21 +136,13 @@ class ViewModelMock constructor(
     override val activatePiP: LiveData<Boolean>
         get() = MutableLiveData(null)
 
-    override fun onPlayClick() {
-    }
+    override fun onPlayClick() = Unit
 
-    override fun onPauseClick() {
-    }
+    override fun onPauseClick() = Unit
 
-    override fun onRestartClick() {
-        TODO("Not yet implemented")
-    }
+    override fun onRestartClick() = Unit
 
-    override fun isPiPActive(inPictureInPictureMode: Boolean) {
-        TODO("Not yet implemented")
-    }
+    override fun isPiPActive(inPictureInPictureMode: Boolean) = Unit
 
-    override fun activatePiP(playing: Boolean) {
-        TODO("Not yet implemented")
-    }
+    override fun activatePiP(playing: Boolean) = Unit
 }
